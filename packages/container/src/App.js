c5a3062f00d90589aa6644e9b0fdedede7afb3c3
Aleksandra Kasprzak <aleksandra.kasprzak@espeo.eu>
2021-06-09 16:18:26 +0200

    header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 
 export default () => {
     return (
-    <>
-        <h1>Hi there!!!!!!</h1>
-        <hr />
-        <MarketingApp/>
-    </>
+        <BrowserRouter>
+            <>
+                <Header />
+                <MarketingApp/>
+            </>
+        </BrowserRouter>
     )
 }
