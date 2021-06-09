7e5972532c4d5a8796a423a8e9ccc56204be0467
Aleksandra Kasprzak <aleksandra.kasprzak@espeo.eu>
2021-06-09 16:54:37 +0200

    generate classname css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'container'
+})
 
 
 export default () => {
     return (
-        <BrowserRouter>
-            <>
-                <Header />
-                <MarketingApp/>
-            </>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <>
+                    <Header />
+                    <MarketingApp/>
+                </>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
